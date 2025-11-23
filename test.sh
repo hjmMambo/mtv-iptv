966f82eb834fddedde63e52443d310b6d9fdc4ec
hjmMambo <d996456879@gmail.com>
2025-11-23 15:37:25 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,56 +12,43 @@ if [[ "${NAME}" == *Alpine* ]]; then
 
 	domain_list=()
 	echo -e "\033[1;32m----自签证书生成----\033[0m"
-	
-	for (( i=1; i<=2; i++)); do
-		if (( i == 1 )); then
-			echo -n "输入面板的主域名："
-			read address
-		else
-			echo -n "输入节点的域名："
-			read address
-		fi
-		
+
+	echo -e "\033[1;32m选项：\033[0m"
+	echo -n "1：签发1个域名证书（面板和节点使用同一个证书,默认选项）"
+	echo -n "2：签发2个不同的域名证书（面板和节点各一个）"
+	read address_choose
+	address_choose=${address_choose:-1}
+
+	if (( ${address_choose} == 1 )); then
+		echo -n "输入域名："
+		read address
+
 		certs_path="/certs/${address}_ecc"
 		mkdir -p ${certs_path}
 		openssl genrsa -out ${certs_path}/server.key 2048
-		openssl req -new -key ${certs_path}/server.key -out ${certs_path}/server.csr -subj "/CN=${address}"
+		openssl req -new -key ${certs_path}/server.key -out ${certs_path}/server.csr -subj "/CN=*.${address}"
 		openssl x509 -req -in ${certs_path}/server.csr -out ${certs_path}/server.crt -signkey ${certs_path}/server.key -days 3650 > /dev/null 2>&1
 		chmod 604 ${certs_path}/server.key
-		domain_list+=("${address}")			# 添加域名到数组中
-	done
-	for (( i=0; i<${#domain_list[@]}; i++ )); do
-		echo -e "\033[1;32m自签证书生成成功，证书路径：/certs/${domain_list[i]}_ecc\033[0m"
-	done
+		echo -e "\033[1;32m自签证书生成成功，证书路径：/certs/${address}_ecc\033[0m"
+		echo -e "\033[1;32m自动配置caddy文件...\033[0m"
+		
+		caddy_file="/etc/caddy/Caddyfile"				# 获取caddy配置文件的路径
 	
-
-	
-	echo -e "\033[1;32m自动配置caddy文件...\033[0m"
-	
-	caddy_file="/etc/caddy/Caddyfile"
-
-	grpc_path=$(cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 8)
-	cat << EOF > ${caddy_file}
-${domain_list[0]} {
-	tls /certs/${domain_list[0]}_ecc/server.crt /certs/${domain_list[0]}_ecc/server.key
-	@sui path /panel*
+		grpc_path=$(cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 8)		# 生成随机的gprc路径
+		cat << EOF > ${caddy_file}
+*.${address} {
+	tls /certs/${address}_ecc/server.crt /certs/${address}_ecc/server.key
+	@sui path /suipanelweb*
 	handle @sui {
 		reverse_proxy localhost:12345
 	}
 
-	handle {
-		redir https://bing.com
-	}
-}
-
-${domain_list[1]} {
-	tls /certs/${domain_list[1]}_ecc/server.crt /certs/${domain_list[1]}_ecc/server.key
 	@grpc path /${grpc_path}*
 	handle @grpc {
 		reverse_proxy localhost:20001 {
 			transport http {
-                versions h2c
-            }
+	            versions h2c
+	        }
 		}
 	}
 	
@@ -70,6 +57,67 @@ ${domain_list[1]} {
 	}
 }
 EOF
+		
+	elif (( ${address_choose} == 2 )); then
+		for (( i=1; i<=2; i++)); do
+			if (( i == 1 )); then
+				echo -n "输入面板的主域名："
+				read address
+			else
+				echo -n "输入节点的域名："
+				read address
+			fi
+			
+			certs_path="/certs/${address}_ecc"
+			mkdir -p ${certs_path}
+			openssl genrsa -out ${certs_path}/server.key 2048
+			openssl req -new -key ${certs_path}/server.key -out ${certs_path}/server.csr -subj "/CN=${address}"
+			openssl x509 -req -in ${certs_path}/server.csr -out ${certs_path}/server.crt -signkey ${certs_path}/server.key -days 3650 > /dev/null 2>&1
+			chmod 604 ${certs_path}/server.key
+			domain_list+=("${address}")			# 添加域名到数组中
+		done
+		for (( i=0; i<${#domain_list[@]}; i++ )); do
+			echo -e "\033[1;32m自签证书生成成功，证书路径：/certs/${domain_list[i]}_ecc\033[0m"
+		done
+		
+	
+		
+		echo -e "\033[1;32m自动配置caddy文件...\033[0m"
+		
+		caddy_file="/etc/caddy/Caddyfile"
+	
+		grpc_path=$(cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 8)
+		cat << EOF > ${caddy_file}
+${domain_list[0]} {
+	tls /certs/${domain_list[0]}_ecc/server.crt /certs/${domain_list[0]}_ecc/server.key
+	@sui path /panel*
+	handle @sui {
+		reverse_proxy localhost:12345
+	}
+	
+	handle {
+		redir https://bing.com
+	}
+}
+	
+${domain_list[1]} {
+	tls /certs/${domain_list[1]}_ecc/server.crt /certs/${domain_list[1]}_ecc/server.key
+	@grpc path /${grpc_path}*
+	handle @grpc {
+		reverse_proxy localhost:20001 {
+			transport http {
+	            versions h2c
+	        }
+		}
+	}
+		
+	handle {
+		redir https://bing.com
+	}
+}
+EOF
+	fi
+	
 	
 	caddy_status=$(rc-service caddy status)
 	if [[ ${message} == " * status: started" ]]; then
