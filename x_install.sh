92dae91de40e6ca44a080b8028ab361bc9314a30
hjmMambo <d996456879@gmail.com>
2025-11-25 01:31:14 +0800

    Update x_install.sh

--- x_install.sh
+++ x_install.sh
@@ -75,7 +75,7 @@ touch /usr/local/etc/xray/config.json
 # 生成uuid
 uuid=$(xray uuid)
 # 生成路径
-vless_path=$(cat /dev/urandom | td -cd 'a-zA-Z0-9' | head -c 8)
+vless_path=$(cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 8)
 echo -e "${green}vless_path路径：${vless_path}${colorend}"
 
 echo -n "输入域名："
