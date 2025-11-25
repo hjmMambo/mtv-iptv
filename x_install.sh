156797b6f07e527f60af430cf336a392445f7edf
hjmMambo <d996456879@gmail.com>
2025-11-25 19:20:48 +0800

    Update x_install.sh

--- x_install.sh
+++ x_install.sh
@@ -5,98 +5,64 @@ colorend="\033[0m"
 
 echo -e "${green}安装必要安装包${colorend}"
 apk add bash curl sudo
-
 echo -n "正在安装 openssl"
 apk add openssl
-
-
 echo -n "正在安装 iproute2"
 apk add iproute2
-
-
 echo -n "正在安装 caddy"
 apk add caddy
-
-
-# 下载xray内核
 echo -e "${green}下载 Xray 内核${colorend}"
 wget https://github.com/XTLS/Xray-core/releases/download/v25.10.15/Xray-linux-64.zip
-
-# 解压xray程序到 /usr/local/bin 目录下，方便直接调用
 unzip /root/Xray-linux-64.zip -d /usr/local/bin
-chmod +x /usr/local/bin/xray            # 设置xray执行权限
-
-# 创建xray存放日志文件
+chmod +x /usr/local/bin/xray
 mkdir -p /var/log/xray
 touch /var/log/xray/access.log
 touch /var/log/xray/error.log
-chmod a+w /var/log/xray/*.log	# 设置日志文件权限
-
-
-# 创建OpenRC服务
+chmod a+w /var/log/xray/*.log
 touch /etc/init.d/xray_service
 chmod +x /etc/init.d/xray_service
-
-
-
 cat << \EOF > /etc/init.d/xray_service
 #!/sbin/openrc-run
-
 depend() {
     need net
 }
-
 name="xray"
 description="Xray-core service"
-command="/usr/local/bin/xray"			# xray执行文件路径
-command_args="-c /usr/local/etc/xray/config.json"	# xray的节点配置信息。
+command="/usr/local/bin/xray"
+command_args="-c /usr/local/etc/xray/config.json"
 pidfile="/var/run/xray.pid"
 background="true"
 extra_started_commands="reload"
-
 start() {
     start-stop-daemon --start \
         --exec ${command} \
         --pidfile ${pidfile} \
         --background \
         --make-pidfile \
-        -- ${command_args} # 将 command_args 传递给 Xray 进程
+        -- ${command_args}
 }
-
 stop() {
 	start-stop-daemon --stop \
 	    --pidfile ${pidfile}
 }
-
 reload() {
 	start-stop-daemon --signal HUP \
         --pidfile ${pidfile}
 }
 EOF
-
-
-# 创建节点配置文件
 mkdir -p /usr/local/etc/xray
 touch /usr/local/etc/xray/config.json
 chmod r+w /usr/local/etc/xray/config.json
-
-# 生成uuid
 uuid=$(xray uuid)
-# 生成路径
 vless_path=$(cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 8)
-
 echo -n "输入域名："
 read domain
-
 echo -n "输入端口(默认:20001)："
 read vless_port
 vless_port=${vless_port:-20001}
-
 echo -n "输入要伪装的域名(默认:https://bing.com)："
 read fake_url
 fake_url=${fake_url:-https://bing.com}
-
-
 cat << EOF > /usr/local/etc/xray/config.json
 {
     "log": {
@@ -161,9 +127,6 @@ cat << EOF > /usr/local/etc/xray/config.json
     ]
 }
 EOF
-
-
-# 生成自签证书
 echo -e "${green}------------生成自签证书------------${colorend}"
 mkdir -p /certs/${domain}_ecc/ && 
 cd /certs/${domain}_ecc/ && 
@@ -171,26 +134,18 @@ openssl genrsa -out server.key 2048 &&
 openssl req -new -key server.key -out server.csr -subj "/CN=${domain}" && 
 openssl x509 -req -in server.csr -out server.crt -signkey server.key -days 3650 && 
 chmod 604 server.key
-
-# 配置caddy
 cat << EOF > /etc/caddy/Caddyfile
 ${domain} {
 	tls /certs/${domain}_ecc/server.crt /certs/${domain}_ecc/server.key
-
     @path path /${vless_path}*
     handle @path {
         reverse_proxy localhost:${vless_port}
     }
-	
     handle {
         redir ${fake_url}
     }
 }
 EOF
-
-
-
-# 启动caddy
 caddy_status=$(rc-service caddy status)
 sleep 0.5s
 echo "${green}caddy_status:${caddy_status}${colorend}"
@@ -202,9 +157,6 @@ else
     echo -e "${green}启动 caddy${colorend}"
 	rc-service caddy start
 fi
-
-
-# 启动 x-ray
 xray_status=$(rc-service xray_service status)
 sleep 0.5s
 echo "xray_status:${xray_status}"
@@ -216,6 +168,4 @@ else
     echo -e "${green}启动 xray${colorend}"
 	rc-service xray_service start
 fi
-
-# 节点信息
 echo -e "${green}vless://${uuid}@${domain}:443?encryption=none&security=tls&sni=${domain}&type=ws&path=%2F${vless_path}#vless${colorend}"
