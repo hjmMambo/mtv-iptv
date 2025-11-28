1eb850742c1e408fac0874823c881e105dab558e
hjmMambo <d996456879@gmail.com>
2025-11-28 23:01:19 +0800

    Update install_64m.sh

--- install_64m.sh
+++ install_64m.sh
@@ -13,9 +13,12 @@ echo -n "正在安装 nginx"
 apk add nginx
 # 下载xray内核
 echo -e "${green}下载 Xray 内核${colorend}"
-wget https://github.com/hjmMambo/mtv-iptv/blob/main/Xray-linux-64.zip
+wget https://github.com/hjmMambo/all-goods/blob/master/xray
+# 移动xray到/usr/local/bin
+mv xray /usr/local/bin
+
 # 解压xray程序到 /usr/local/bin 目录下，方便直接调用
-unzip /root/Xray-linux-64.zip -d /usr/local/bin
+# unzip /root/Xray-linux-64.zip -d /usr/local/bin
 chmod +x /usr/local/bin/xray
 mkdir -p /var/log/xray
 touch /var/log/xray/access.log
